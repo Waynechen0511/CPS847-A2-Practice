e5f92cc3716135cc3e93583f6e2ae9646f7030ee
Wayne Chen <wayne.chen@ryerson.ca>
2022-03-11 21:25:21 -0500

    CI

--- tests.py
+++ tests.py
@@ -7,14 +7,16 @@ class TestFunctions(unittest.TestCase):
     """
 
     def test_add2(self):
+        """test add 2"""
         self.assertEqual(add2(2), 4)
         self.assertEqual(add2(3), 5)
         self.assertEqual(add2(4), 6)
 
     def test_add3(self):
+        """test add 3"""
         self.assertEqual(add3(2), 5)
         self.assertEqual(add3(3), 6)
         self.assertEqual(add3(4), 7)
-        
+
 if __name__ == "__main__":
     unittest.main()
