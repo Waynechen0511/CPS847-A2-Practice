498995adb39b627d33a66e745ad9a434ee5035bd
Wayne Chen <wayne.chen@ryerson.ca>
2022-03-11 21:12:06 -0500

    Added workflows and unittests

---
+++ tests.py
@@ -0,0 +1,20 @@
+import unittest
+from Functions import add2, add3
+
+class TestFunctions(unittest.TestCase):
+    """
+    Test Harness
+    """
+
+    def test_add2(self):
+        self.assertEqual(add2(2), 4)
+        self.assertEqual(add2(3), 5)
+        self.assertEqual(add2(4), 6)
+
+    def test_add3(self):
+        self.assertEqual(add3(2), 5)
+        self.assertEqual(add3(3), 6)
+        self.assertEqual(add3(4), 7)
+        
+if __name__ == "__main__":
+    unittest.main()
